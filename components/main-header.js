fcb798de578be664fc19c78b65e0e3215dc24f3c
lhs <sun@lhss-MacBook-Pro.local>
2025-12-22 16:46:29 +0900

    add header-background

--- components/main-header.js
+++ components/main-header.js
@@ -3,10 +3,16 @@ import logoImg from '@/assets/logo.png' // next/image 사용할 땐 logoImg.src 
 
 import classes from './main-header.module.css'
 import Image from "next/image"; // srcset 기기나 뷰포인트 에 맞게 사이즈가 조졍됨, png -> webp, lazy loading -> 깜박이 없이 바로 로딩을 위해 priority
+import MainHeaderBackgroundPage from "./main-header-background";
 
+
+// MainHeaderBackgroundPage 를 rootRayout 에 넣을 수도 있지만 관심사를 모으기 위해 여기에 넣는다
+// 형제요소를 넣기 위해선 <>{형제 요소를 여기에 넣어야 한다}</> 
 
 export default function MainHeader() {
     return (
+    <>
+        <MainHeaderBackgroundPage />
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
                 <Image src={logoImg} alt="A plate with food on it" priority/>
@@ -23,7 +29,9 @@ export default function MainHeader() {
                     </li>
                 </ul>
             </nav>
-        </header>    
+        </header>  
+    </>
+          
     )
     
 }
