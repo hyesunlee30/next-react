34626377bcd22be64836647db34aa83fe40c71b7
lhs <sun@lhss-MacBook-Pro.local>
2025-12-22 16:30:39 +0900

    use next-js Image

--- components/main-header.js
+++ components/main-header.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
-import logoImg from '@/assets/logo.png'
+import logoImg from '@/assets/logo.png' // next/image 사용할 땐 logoImg.src 가 아닌 logoImg 객체 전체여야 한다 
 
 import classes from './main-header.module.css'
+import Image from "next/image"; // srcset 기기나 뷰포인트 에 맞게 사이즈가 조졍됨, png -> webp, lazy loading -> 깜박이 없이 바로 로딩을 위해 priority
 
 
 export default function MainHeader() {
     return (
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
-                <img src={logoImg.src} alt="A plate with food on it" />
+                <Image src={logoImg} alt="A plate with food on it" priority/>
                 NextLevel Food
             </Link>
 
