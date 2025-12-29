a75f3d2c1c4b14b5364d7e0e37295ea8bf400515
lhs <sun@lhss-MacBook-Pro.local>
2025-12-29 15:25:19 +0900

    separate server action into separate files

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,24 +1,10 @@
+'use client';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-
-  async function shareMeal(formData) {
-    'use server'; // form action 에서 server action, 오직 서버에서만 실행을 보장, nextJs 서버로 보냄
-
-    // name으로 데이터 가져오기
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"), //ImagePicker 에 label과 name 추가후 name으로 가져온다
-      creator: formData.get("name"),
-      creator_email: formData.get("email")
-    }
-
-    console.log(meal);
-
-  }
 
   return (
     <>
