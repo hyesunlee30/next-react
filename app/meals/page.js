c021e63d1abd198c4563e6344f0d9e979f1c124e
lhs <sun@lhss-MacBook-Pro.local>
2025-12-29 10:34:51 +0900

    loading page for better user experience

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,26 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+
+// loading.js 와 loading.module.css를 써서 전역으로 사용하는 방법도 있지만
+// loading 을 보여주고 싶은 곳에서 suspense 와 fallback 을 이용해 필요한 곳에서만 사용하는 것이 좀 더 나은 유저경험이다.
+
+// 데이터 가져오는 부분을 아웃소싱
+// Suspense는 리액트에서 제공하는 컴포넌트로 일부 데이터 또는 리소스가 불리울 때까지 로딩 상태를 처리하고 대체 컨텐츠를 표시할 수 있다.
+// NextJS는 이 Suspense 컴포넌트와 리액트 컨셉을 포용하고 
+// 데이터를 fetching 하고 이런 promise 를 반환한다.
+async function Meals() {
 
     const meals = await getMeals();
+
+    return <MealsGrid meals={meals}/>
+}
+
+export default function MealsPage() {
+
+    
 
     return (
         <>
@@ -24,7 +40,10 @@ export default async function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals}/>
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
+                
             </main>
         </>
     );
